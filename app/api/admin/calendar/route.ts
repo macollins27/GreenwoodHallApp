8e3ca635b5a59e27d17cecb5e0d8a4215df5150a
macollins27 <collinsamax@gmail.com>
2025-11-19 11:01:19 -0500

    Fix calendar date range query

--- app/api/admin/calendar/route.ts
+++ app/api/admin/calendar/route.ts
@@ -29,7 +29,7 @@ export async function GET(request: Request) {
       where: {
         eventDate: {
           gte: start,
-          lte: end,
+          lt: end,
         },
       },
       orderBy: {
